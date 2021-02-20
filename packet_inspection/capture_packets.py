0b93e22429fbfc1285e7a4bfecf2c2c02ff67215
briweinstein <bweinstein23@gmail.com>
2021-02-20 05:50:40 +0000

    Finalized argument parsing and set up packet capture options.

--- packet_inspection/capture_packets.py
+++ packet_inspection/capture_packets.py
@@ -2,6 +2,9 @@
 from scapy import *
 import argparse, re, sys
 
+# Validated MAC addresses given as input
+mac_addrs = []
+
 # TODO: Does the router need to be in monitor mode?
 
 ##############################################################################################
@@ -15,15 +18,18 @@ import argparse, re, sys
 def main():
   # 1) Parse and validate arguments
   print("Parsing arguments")
-  mac_addrs = parse_validate_arguments()
-  
+  parse_validate_arguments()
+  print(mac_addrs)
+
   # 2) Capture IoT packets only
   print("Capturing IoT packets only")
   # Sniff
   #sniff(iface="mon0",prn=packet_capture)
-  
+  # https://stackoverflow.com/questions/24386000/how-to-filter-by-ethernet-mac-address
+  #sniff(lfilter=lambda d: d.src in mac_addrs, prn=packet_capture)
+
   # 3) Parse packets
-  #parse_packets()
+  #packet_parse()
 
 ##############################################################################################
 ### Parse and validate arguments
@@ -35,28 +41,35 @@ def parse_validate_arguments():
   parser.add_argument("mac_addresses", nargs="+", help="A list of MAC addresses belonging to IoT devices to monitor.")
   args = parser.parse_args()
 
-  # Throw an error on a bad MAC address
   # TODO: Not sure if the package can throw errors like this @Martin
   for addr in args.mac_addresses:
+    # Throw an error on a bad MAC address
     # https://stackoverflow.com/questions/7629643/how-do-i-validate-the-format-of-a-mac-address
-    if not re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", addr.lower()):
+    if not re.match("[0-9a-f]{2}([:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", addr.lower()):
       sys.exit("Provided address " + addr + " is not a vaid MAC address.")
+
+  global mac_addrs
+  mac_addrs = args.mac_addresses
 
 ##############################################################################################
 ### Capture IoT packets only
 ##############################################################################################
 
 # Take in a list of confirmed MAC addresses to captue packets for
-def packet_capture():
-  # Stuff
+def packet_capture(packet):
+  print("IoT MAC Addresses: " + mac_addrs)
+  packet_mac = packet.info + "----" + packet.addr2
+  print("Packet MAC maybe: " + packet_mac)
+  #print(packet)
 
 ##############################################################################################
 ### Parse packets
 ##############################################################################################
 
 # Parse the IoT packets
-def parse_packets():
+def packet_parse():
   # Stuff
+  print("hello")
 
 ##############################################################################################
 ### Call main()
@@ -66,6 +79,6 @@ main()
 
 # Sources:
 # https://docs.python.org/3/library/argparse.html
-
+# https://linuxsecurityblog.com/2016/02/04/sniffing-access-points-and-mac-addresses-using-python/
 
 
