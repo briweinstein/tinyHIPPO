51e217def720f5eeeb9a576f108ea894a79c8568
samir-elhelw76 <elhelwsamir@gmail.com>
2021-03-03 00:22:41 -0500

    IPSignature can distinguish between incoming and outgoing IP packets

--- signature_detection/IPSignature.py
+++ signature_detection/IPSignature.py
@@ -1,11 +1,11 @@
-import Signature
+from .Signature import Signature
 import ipaddress
 import scapy.layers.inet as net
 
 
-class IPSignature(Signature.Signature):
+class IPSignature(Signature):
     '''
-    - subnet: local ip space for this network
+        - subnet: local ip space for this network
     '''
 
     def __init__(self, subnet: str):
@@ -18,8 +18,8 @@ class IPSignature(Signature.Signature):
         # check whether source ip is private
         if not ipaddress.ip_address(ip_layer.src).is_private:
             # TODO: if not private check whether it's from a trusted source eg check virustotal
-            print('incoming packet:%s')
+            print(f'incoming packet: {ip_layer.src}')
         else:
             # TODO:if outgoing check mac addresses against config and ensure it's coming from a user verified space
             # TODO: if outgoing
-            print('outgoing packet')
+            print(f'outgoing packet: {ip_layer.src}')
--- signature_detection/Signature.py
+++ signature_detection/Signature.py
@@ -1,9 +1,10 @@
 import abc
+from scapy.all import Packet
 
 '''This abstract base class represents a single signature to be checked against packets in our IoT IDS'''
 
 
 class Signature(abc.ABC):
     @abc.abstractmethod
-    def __call__(self, packet: str):
+    def __call__(self, packet: Packet) -> bool:
         raise NotImplementedError
