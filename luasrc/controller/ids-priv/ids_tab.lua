d3e931b54ef839804ad5e6d6d551272885197799
Lucas <mccanna.l@husky.neu.edu>
2021-03-10 13:05:50 -0500

    Alert dev (#6)
    Completed dashboard functionality 
    
    * Added dashboard template and alert object, updated email to allow for alert object
    
    * Copied VM makefile over to repo
    
    * Removed old directories
    
    * Fixed file saving issue, JS properly formats MAC addresses
    
    * Added alert collection functionality and frontend usage
    
    * Removed default constructor

--- luasrc/controller/ids-priv/ids_tab.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.ids-priv.ids_tab", package.seeall)  --notice that new_tab is the name of the file new_tab.lua
- function index()
-     entry({"admin", "ids_tab"}, firstchild(), "Alert Dashboard", 60).dependent=false  
-     entry({"admin", "ids_tab", "ids_alerts"}, template("ids-priv/ids_page"), "IDS Dashboard" , 1)  
-     entry({"admin", "ids_tab", "priv_alerts"}, template("ids-priv/priv_page"), "Privacy Dashboard", 2)  
-end
