2b921a71054ac017ae1bc6c1d86952daa39eb69b
briweinstein <bweinstein23@gmail.com>
2021-03-16 19:08:29 +0000

    first alert in

--- src/privacy_analysis/system_analysis/system_privacy_encryption.py
+++ src/privacy_analysis/system_analysis/system_privacy_encryption.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 from src.privacy_analysis.system_analysis.system_privacy import SystemPrivacy
 from src.privacy_analysis.system_analysis.helpers_analysis import get_file_contents
-
+from dashboard.alerts.alert import alert, ALERT_TYPE, SEVERITY
 
 # Validate the router encryption type is not weak
 class SystemPrivacyEncryption(SystemPrivacy):
@@ -14,5 +14,9 @@ class SystemPrivacyEncryption(SystemPrivacy):
             return
         for mode in weak_encryption_modes:
             if ("encryption '" + mode) in data:
-                # TODO: alert("Weak encryption is in use. Switch to WPA2 from " + mode + ".")
                 print("Weak encryption found")
+                alert_obj = alert("Weak encryption is in use. Switch to WPA2 from " + mode + ".", ALERT_TYPE.PRIVACY, SEVERITY.ALERT)
+                alert_obj.alert()
+                print("Weak encryption alerted")
+
+
