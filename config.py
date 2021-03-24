588e5b6acabfc2e863b1f66592eb032f04f0cf0d
Martin Petrauskas <58351012+MartinLPetrauskas@users.noreply.github.com>
2021-03-24 09:12:43 -0400

    Prototype deployment (#16)
    * Renaming from cids -> tinyHIPPO and adding bugfixes and relevant changes
    
    * Fixed typo bug in config.py and do not check for recipient email for now
    
    * Temporarily removed a circular import which effectively disables the email system for now
    
    * Temporarily removed when exceptions are raised since they cause the program to crash
    
    * Modified tiny_hippo_run.py to sniff all the packets for testing purposes
    
    * Config now checks for recipient email again
    
    * Fixed filepath errors which caused dashboard to not load successfully
    
    * Removing opkg install calls from start
    
    * Adding logging instead of printing to console or raising exceptions
    
    * Removed checking of default values as requested by Samir
    
    * Small fix for tinyHIPPOstart
    
    * Didn't delete everything.....whoops
    
    * Undoing a change to avoid merge conflicts with current PRs
    
    * logging object created inside __init__ now
    
    * Logging calls are not centralized in tiny_hippo_run
    
    * Raising exceptions again since logging is centralized in tiny_hippo_run
    
    * Fixing logic for try except

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import json
 import os
+import logging
 from typing import List
 from dataclasses import dataclass
-
 
 @dataclass(frozen=True)
 class ConfigEmail:
@@ -14,6 +14,11 @@ class ConfigEmail:
 
 class Config:
     def __init__(self):
+        logging.basicConfig(level=logging.INFO,
+                            filemode='a',
+                            format='%(asctime)s %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            filename='/etc/tinyHIPPO/tinyHIPPO_error.log')
         config_path = os.getenv("IOT_IDS_CONFIGPATH", "config.json")
         with open(config_path) as f:
             config_json = json.load(f)
@@ -21,3 +26,5 @@ class Config:
         self.mac_addrs = config_json["mac_addrs"]
         self.alert_collection_path = config_json["alert_collection_path"]
         self.virustotal_api_key = config_json["virustotal_api_key"]
+        self.log_event = logging.getLogger()
+        
