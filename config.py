b9f27ff3f7c5a3d1d25fd1360563dec59e5b4805
Martin Petrauskas <martin.petrauskas@protonmail.com>
2021-03-23 20:53:07 -0400

    Removed checking of default values as requested by Samir

--- config.py
+++ config.py
@@ -33,13 +33,4 @@ class Config:
         email_account = config_json['email']['email_account']
         email_key = config_json['email']['email_password']
         recipient_email = config_json['email']['recipient_emails']
-
-        if smtp_server == "smtp.example.com":
-            self.log_event.info('STMP Server not configured\n')
-        if email_account == "openwrt@example.com":
-            self.log_event.info('EMAIL ACCOUNT NOT CONFIGURED\n')
-        if email_key == "super_secure_password":
-            self.log_event.info('Email account password not set\n')
-        if recipient_email[0] == "homeowner@example.com":
-            self.log_event.info('Recipient email not configured\n')
             
