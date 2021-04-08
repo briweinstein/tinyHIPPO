ca8f21e46d20e0a597613f72084d72e99042a7b0
samir-elhelw76 <elhelwsamir@gmail.com>
2021-04-07 23:40:51 -0400

    Changed '-' to '_' in luci code

--- src/dashboard/luasrc/controller/ids-priv/ids_tab.lua
+++ src/dashboard/luasrc/controller/ids-priv/ids_tab.lua
@@ -2,8 +2,8 @@ module("luci.controller.ids-priv.ids_tab", package.seeall)  --notice that new_ta
     function index()
         entry({"admin", "ids_tab"}, firstchild(), "Alert Dashboard", 60).dependent=false  
         entry({"admin", "ids_tab", "settings"}, call("get_page", "/ids-priv/settings/"), "Config", 1)
-        entry({"admin", "ids_tab", "ids-alerts"}, call("get_page", "/ids-priv/ids-alerts/"), "IDS Dashboard" , 2)
-        entry({"admin", "ids_tab", "privacy-alerts"}, call("get_page", "/ids-priv/privacy-alerts/"), "Privacy Dashboard", 3)
+        entry({"admin", "ids_tab", "ids_alerts"}, call("get_page", "/ids-priv/ids-alerts/"), "IDS Dashboard" , 2)
+        entry({"admin", "ids_tab", "privacy_alerts"}, call("get_page", "/ids-priv/privacy-alerts/"), "Privacy Dashboard", 3)
         
         -- Entry for incoming requests to save config, request is sent from the JS in the .HTM file (formvalue set to JSON of config options)
        -- entry({"admin", "ids_tab", "config", "save"}, call("action_save_config")).sysauth=false
