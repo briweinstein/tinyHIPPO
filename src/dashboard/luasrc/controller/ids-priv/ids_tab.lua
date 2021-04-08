ba942658cdaa994b6f23cd2b9a0382828f8b5d1b
samir-elhelw76 <elhelwsamir@gmail.com>
2021-04-08 02:26:53 -0400

    Removed unnecessary lua files, re-did templates to pull html from flask webserver, config still needs to be done

--- src/dashboard/luasrc/controller/ids-priv/ids_tab.lua
+++ src/dashboard/luasrc/controller/ids-priv/ids_tab.lua
@@ -1,21 +1,13 @@
 module("luci.controller.ids-priv.ids_tab", package.seeall)  --notice that new_tab is the name of the file new_tab.lua
     function index()
         entry({"admin", "ids_tab"}, firstchild(), "Alert Dashboard", 60).dependent=false  
-        entry({"admin", "ids_tab", "settings"}, call("get_page", "/ids-priv/settings/"), "Config", 1)
+        entry({"admin", "ids_tab", "settings"}, template("ids-priv/config.htm"), "Config", 1)
         entry({"admin", "ids_tab", "ids_alerts"}, template("ids-priv/ids_page"), "IDS Dashboard" , 2)
-        entry({"admin", "ids_tab", "privacy_alerts"}, call("get_page", "/ids-priv/privacy-alerts/"), "Privacy Dashboard", 3)
+        entry({"admin", "ids_tab", "privacy_alerts"}, template("ids-priv/priv_page"), "Privacy Dashboard", 3)
         
         -- Entry for incoming requests to save config, request is sent from the JS in the .HTM file (formvalue set to JSON of config options)
        -- entry({"admin", "ids_tab", "config", "save"}, call("action_save_config")).sysauth=false
     end
-
-    function get_page(route)
-        local http = require("socket.http")
-        local body, code, headers, status = http.request("localhost:5000"..route)
-        luci.http.prepare_content("text/html")
-        luci.http.write(body)
-    end
-
 
     function action_save_config()
         -- Load content from POST
