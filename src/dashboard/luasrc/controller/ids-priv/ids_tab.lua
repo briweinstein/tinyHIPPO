3071c35c5d6413883254dd92baa4b6eb8111a2e3
samir-elhelw76 <elhelwsamir@gmail.com>
2021-04-07 22:16:38 -0400

    Added redirects to flask app

--- src/dashboard/luasrc/controller/ids-priv/ids_tab.lua
+++ src/dashboard/luasrc/controller/ids-priv/ids_tab.lua
@@ -1,14 +1,22 @@
 module("luci.controller.ids-priv.ids_tab", package.seeall)  --notice that new_tab is the name of the file new_tab.lua
     function index()
         entry({"admin", "ids_tab"}, firstchild(), "Alert Dashboard", 60).dependent=false  
-        entry({"admin", "ids_tab", "config"}, template("ids-priv/config"), "Config", 1)
-        entry({"admin", "ids_tab", "ids_alerts"}, template("ids-priv/ids_page"), "IDS Dashboard" , 2)  
-        entry({"admin", "ids_tab", "priv_alerts"}, template("ids-priv/priv_page"), "Privacy Dashboard", 3) 
+        entry({"admin", "ids_tab", "settings"}, call("get_page", "ids-priv/settings"), "Config", 1)
+        entry({"admin", "ids_tab", "ids-alerts"}, call("get_page", "ids-priv/ids-alerts"), "IDS Dashboard" , 2)
+        entry({"admin", "ids_tab", "privacy-alerts"}, call("get_page", "ids-priv/privacy-alerts"), "Privacy Dashboard", 3)
         
         -- Entry for incoming requests to save config, request is sent from the JS in the .HTM file (formvalue set to JSON of config options)
-        entry({"admin", "ids_tab", "config", "save"}, call("action_save_config")).sysauth=false
+       -- entry({"admin", "ids_tab", "config", "save"}, call("action_save_config")).sysauth=false
     end
-    
+
+    function get_page(route)
+        local http = require("socket.http")
+        local body, code, headers, status = http.request("localhost:5000"..route)
+        luci.http.prepare_content("text/html")
+        luci.http.write(body)
+    end
+
+
     function action_save_config()
         -- Load content from POST
         local config, config_length = luci.http.content()
