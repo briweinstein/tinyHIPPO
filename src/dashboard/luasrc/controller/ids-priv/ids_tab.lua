ebb88407419d286b63e8e27144d1d0520e612c4c
samir-elhelw76 <elhelwsamir@gmail.com>
2021-04-08 16:24:14 -0400

    Added logic to display neighboring devices using flask

--- src/dashboard/luasrc/controller/ids-priv/ids_tab.lua
+++ src/dashboard/luasrc/controller/ids-priv/ids_tab.lua
@@ -1,7 +1,7 @@
 module("luci.controller.ids-priv.ids_tab", package.seeall)  --notice that new_tab is the name of the file new_tab.lua
     function index()
         entry({"admin", "ids_tab"}, firstchild(), "Alert Dashboard", 60).dependent=false  
-        entry({"admin", "ids_tab", "settings"}, template("ids-priv/config.htm"), "Config", 1)
+        entry({"admin", "ids_tab", "settings"}, template("ids-priv/config"), "Config", 1)
         entry({"admin", "ids_tab", "ids_alerts"}, template("ids-priv/ids_page"), "IDS Dashboard" , 2)
         entry({"admin", "ids_tab", "privacy_alerts"}, template("ids-priv/priv_page"), "Privacy Dashboard", 3)
         
--- src/dashboard/webserver/server.py
+++ src/dashboard/webserver/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, g
 from src import run_config
-from src.dashboard.webserver.server_utils import get_db, get_alerts
+from src.dashboard.webserver.server_utils import get_db, get_neighboring_devices, get_alerts
 
 app = Flask(__name__)
 
@@ -12,7 +12,8 @@ def init_db():
 
 @app.route('/ids-priv/settings/')
 def settings():
-    pass
+    ip_neighbors = get_neighboring_devices()
+    return render_template('config.html', neighboring_devices=ip_neighbors)
 
 
 @app.route('/ids-priv/ids-alerts/')
@@ -25,3 +26,7 @@ def ids_alerts():
 def privacy_alerts():
     all_alerts = get_alerts('Privacy', g.db)
     return render_template('alerts.htm', all_alerts=all_alerts, dashboard_title='Privacy')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
