5e0675f2df4c0e1370efe6ba1fff5815b1d53f37
Lucas <mccanna.l@husky.neu.edu>
2021-04-09 14:20:37 -0400

    Fixing deviation

--- src/anamoly_detection/frequency_signatures/classifications/ip_camera_frequency_signature.py
+++ src/anamoly_detection/frequency_signatures/classifications/ip_camera_frequency_signature.py
@@ -7,41 +7,34 @@ from src.dashboard.alerts.alert import Alert, ALERT_TYPE, SEVERITY
 from src.anamoly_detection.frequency_signatures.abstract_frequency_signature import AbstractFrequencySignature
 
 class IPCameraFrequencySignature(AbstractFrequencySignature):
-    def __init__(self, equation, window_size=3600, interval_size=600):
+    def __init__(self, equation, dev_equation, window_size=3600, interval_size=600):
+        # Lambda function equations
         self._limit_equation = equation
-        self._window_frequency = 0
-        self._interval_frequencies = deque(maxlen=(ceil(window_size / interval_size)))
+        self._deviation_equation = dev_equation
 
         # Sizes used to evaluate when to shift the window
         self._window_size = window_size
         self._interval_size = interval_size
 
-    def get_window_frequency(self):
-        return self._window_frequency
+    def get_window_size(self):
+        return self._window_size
 
-    def _set_window_frequency(self, value: int):
-        self._window_frequency = value
-
-    def get_intervals(self):
-        return self._interval_frequencies
-
-    def _set_intervals(self, value: deque):
-        self._interval_frequencies = value
+    def get_interval_size(self):
+        return self._interval_size
 
     def get_limit_equation(self):
         return self._limit_equation
 
-    def _set_limit_equation(self, value):
-        self._limit_equation = value
+    def get_deviation_equation(self):
+        return self._deviation_equation
 
     def __call__(self, packet: Packet):
         self._window_frequency += 1
-        hour = (packet.time % 86400) // self._window_size
-        expected_average = self._limit_equation()
+        hour = (packet.time % 86400) / self._window_size
+        expected_average = self._limit_equation(hour, self._interval_size)
+        deviation = self._deviation_equation(hour, self._interval_size)
 
-        # TODO: Implement std deviation equation
-
-        if expected_average > self._window_frequency:
+        if expected_average > self._window_frequency + deviation:
             dst = False
             if packet["Ethernet"].src not in run_config.mac_addrs:
                 dst = True
