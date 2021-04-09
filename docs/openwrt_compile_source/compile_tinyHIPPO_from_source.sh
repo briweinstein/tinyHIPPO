d6bebc5990936f894a1b7f1a6f1052df2ff2ab53
Martin Petrauskas <58351012+MartinLPetrauskas@users.noreply.github.com>
2021-04-09 14:19:25 -0400

    Adding documentation on how to compile the package from source (#27)
    * Adding documentation on how to compile the package from source
    
    * Removed old comment

---
+++ docs/openwrt_compile_source/compile_tinyHIPPO_from_source.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Uninstall the old tinyHIPPO package
+echo 'Uninstalling old tinyHIPPO package'
+$HOME/source/scripts/feeds uninstall tinyHIPPO
+
+
+echo 'Going into the repo and changing to the main branch'
+cd $HOME/OpenWrt-IoT-IDS-Privacy && git checkout main
+
+echo 'Removing old files in tinyHIPPO'
+rm -r $HOME/mypackages/examples/tinyHIPPO/src/*
+
+echo 'Copying over the new files'
+cp -r $HOME/OpenWrt-IoT-IDS-Privacy/* $HOME/mypackages/examples/tinyHIPPO/src
+
+echo 'Updating package feeds to include new tinyHIPPO'
+$HOME/source/scripts/feeds update mypackages
+
+echo 'Installing the new tinyHIPPOdev in the package feeds'
+$HOME/source/scripts/feeds install -a -p mypackages
+
+echo 'Running make menuconfig'
+cd $HOME/source/ && make menuconfig
+
+echo 'COMPILING'
+cd $HOME/source/ && sudo make package/tinyHIPPO/compile V=sc
