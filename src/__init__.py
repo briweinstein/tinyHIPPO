58969a6337cd7d1d9f60b461ba329b1d4bb34587
Lucas <mccanna.l@husky.neu.edu>
2021-04-17 17:53:58 -0400

    Fix import issue

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,10 @@
 from config import Config
 from .database.db_connection import DBConnection
-from src.anamoly_detection.anomaly_engine import AnomalyEngine
 
 run_config = Config()
 try:
     db = DBConnection(run_config.db_file)
     db.create_session()
-    anomaly_engine = AnomalyEngine(db)
 except Exception as e:
-    db = None
     run_config.log_event.info(f"Database connection error {e}")
-try:
-    anomaly_engine = AnomalyEngine(db)
-except Exception as e:
-    anomaly_engine = AnomalyEngine(None)
-    run_config.log_event.info(f"Anomaly Engine initialization error: {e}")
+
