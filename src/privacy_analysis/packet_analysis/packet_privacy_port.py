6444f60fc8531c462aabda38d7290daae9eacb38
Brianna <bweinstein23@gmail.com>
2021-04-25 20:05:30 -0400

    Updated privacy alerts with MAC and IP

--- src/privacy_analysis/packet_analysis/packet_privacy_port.py
+++ src/privacy_analysis/packet_analysis/packet_privacy_port.py
@@ -1,7 +1,9 @@
 import re
+
 from src import run_config
 from src.privacy_analysis.packet_analysis.packet_privacy import PacketPrivacy
-from scapy.layers.inet import TCP, UDP
+from scapy.layers.inet import TCP, UDP, IP
+from scapy.layers.l2 import Ether
 from scapy.all import Packet
 from src.dashboard.alerts.alert import Alert, AlertType, Severity
 
@@ -38,21 +40,30 @@ class PacketPrivacyPort(PacketPrivacy):
             # Scan for using port 80 and the plaintext for privacy leaks
             if (self.packet[proto_type].dport == 80) or (self.packet[proto_type].sport == 80):
                 alert_port_80 = Alert(None, "Sending data over unencrypted port.", AlertType.PRIVACY, Severity.ALERT)
+                if self.packet[proto_type].dport == 80:
+                    vuln_target = "dst"
+                else:
+                    vuln_target = "src"
+                alert_port_80.device_mac = self.packet[Ether][vuln_target]
+                alert_port_80.device_ip = self.packet[IP][vuln_target]
                 alert_port_80.alert()
-                self.__scan_plaintext(proto_type)
+                self.__scan_plaintext(proto_type, vuln_target)
 
             # Monitor suspicious ports
             if self.packet[proto_type].dport in suspicious_ports:
                 alert_suspicious_ports = Alert(None, "Suspicious destination port used: " +
                                                str(self.packet[proto_type].dport), AlertType.PRIVACY, Severity.WARN)
+                alert_suspicious_ports.device_mac = self.packet[Ether].dst
+                alert_suspicious_ports.device_ip = self.packet[IP].dst
                 alert_suspicious_ports.alert()
 
-    def __scan_plaintext(self, proto_type):
+    def __scan_plaintext(self, proto_type, vuln_target):
         """
         Scan plaintext for privacy leaks and alert on findings: credit cards, social security numbers, emails,
         and suspicious keywords. Note that there is a chance for false positives with credit cards and SSNs,
         because they're just numbers
         :param proto_type: Either TCP or UDP
+        :param vuln_target: Either "src" or "dst", depending on where the plaintext vulnerability originiated from
         """
         # Try to get the payload
         try:
@@ -62,31 +73,38 @@ class PacketPrivacyPort(PacketPrivacy):
             return
 
         # Use a regex to look for credit cards, needs a non-number before and after the numbers
-        self.__regex_alert("[\D](?:[0-9]{4}-){3}[0-9]{4}[\D]|[\D][0-9]{16}[\D]", "Credit card information found in a plaintext packet.")
+        self.__regex_alert("[\D](?:[0-9]{4}-){3}[0-9]{4}[\D]|[\D][0-9]{16}[\D]", "Credit card information found in a "
+                                                                                 "plaintext packet.", vuln_target)
 
         # Use a regex to look for SSNs, needs a non-number before and after the numbers
-        self.__regex_alert("[\D][0-9]{9}[\D]|[\D][0-9]{3}-[0-9]{2}-[0-9]{4}[\D]", "SSN information found in a plaintext packet.")
+        self.__regex_alert("[\D][0-9]{9}[\D]|[\D][0-9]{3}-[0-9]{2}-[0-9]{4}[\D]", "SSN information found in a "
+                                                                                  "plaintext packet.", vuln_target)
 
         # Use a regex to look for emails - this is not a huge privacy leak, but still mentionable
         # The email will not be included in the alert for privacy reasons
-        self.__regex_alert("[^@]+@[^@]+\.[^@]+", "Email information found in a plaintext packet.")
+        self.__regex_alert("[^@]+@[^@]+\.[^@]+", "Email information found in a plaintext packet.", vuln_target)
 
         # Search for specific words to alert on
         for keyword in suspicious_strings:
             if keyword in self.payload:
                 alert_keyword = Alert(None, f'Suspicious keyword found in a plaintext packet: {keyword}',
                                       AlertType.PRIVACY, Severity.ALERT)
+                alert_keyword.device_mac = self.packet[Ether][vuln_target]
+                alert_keyword.device_ip = self.packet[IP][vuln_target]
                 alert_keyword.alert()
 
-    def __regex_alert(self, regex_string, alert_string):
+    def __regex_alert(self, regex_string, alert_string, vuln_target):
         """
         Scan the payload with the given regex string and alert if necessary
         :param regex_string: The regex string to scan the paylaod with
         :param alert_string: The string to pass into the Alert object as the description for the event
+        :param vuln_target: Either "src" or "dst", depending on where the plaintext vulnerability originiated from
         """
         if re.search(regex_string, self.payload):
-            alert_search_email = Alert(None, alert_string, AlertType.PRIVACY, Severity.ALERT)
-            alert_search_email.alert()
+            alert_regex = Alert(None, alert_string, AlertType.PRIVACY, Severity.ALERT)
+            alert_regex.device_mac = self.packet[Ether][vuln_target]
+            alert_regex.device_ip = self.packet[IP][vuln_target]
+            alert_regex.alert()
 
 
 """
